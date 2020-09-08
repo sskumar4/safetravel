598682976e64458c76609906c20850d99be8aa23
Sudha SK <sskumar4@ncsu.edu>
2020-09-08 12:58:30 -0400

    added models, updated config.json to match heroku jaws db, added initial routes, created html etc.,

---
+++ models/city.js
@@ -0,0 +1,30 @@
+module.exports = function(sequelize, DataTypes) {
+  var City = sequelize.define("City", {
+    // The email cannot be null, and must be a proper email before creation
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      //unique: true,
+      // validate: {
+      //   isEmail: true
+      // }
+    },
+    // The safety rating score received from amadeus API
+    ratingScore: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+  City.associate = function(models) {
+    // We're saying that a City should belong to an User
+    // A City can't be created without an User due to the foreign key constraint
+    City.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+
+  return City;
+};
---
+++ public/js/members.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/user_data").then(function(data) {
+    $(".member-name").text(data.email);
+  });
+add API code and addEventListener ajax calls
+
+add the jquery code to display the safety score
+
+Addwishlist button makes another ajax call
+
+
+});
